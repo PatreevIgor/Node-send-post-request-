b70131e4435c711cce74007794f0322697aa284b
Patreev Igor <Patreewigor@yandex.ru>
2019-02-28 10:24:41 +0300

    Update request.js

--- request.js
+++ request.js
@@ -1,26 +1,25 @@
-var request = require('request');
+var postData =  'xml;'
+const soapRequest = require('easy-soap-request');
 
-// To post JSON data:
-var myJSONObject = { ... };
-request({
-    url: "http://josiahchoi.com/myjson",
-    method: "POST",
-    json: true,   // <--Very important!!!
-    body: myJSONObject
-}, function (error, response, body){
-    console.log(response);
-});
+// example data
+const url = '';
+const headers = {
+         'Host': '10.117.10.201:8181',
+         'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; Win64; x64; rv:65.0) Gecko/20100101 Firefox/65.0',
+         'Accept': '*/*',
+         'Accept-Language': 'ru-RU,ru;q=0.8,en-US;q=0.5,en;q=0.3',
+         'Accept-Encoding': 'gzip, deflate',
+         'soapaction': 'http://bus.colvir.com/service/cas/v4/auth',
+         'content-type': 'application/xml',
+         'Origin': 'moz-extension://96cb16bb-381b-4496-a51d-7b7247826dcc',
+        'Content-Length': '2565',
+         'Connection': 'keep-alive'
+       };
 
-
-// To post xml data:
-var myXMLText = '<xml>...........</xml>'
-request({
-    url: "http://josiahchoi.com/myjson",
-    method: "POST",
-    headers: {
-        "content-type": "application/xml",  // <--Very important!!!
-    },
-    body: myXMLText
-}, function (error, response, body){
-    console.log(response);
-});
+// usage of module
+(async () => {
+  const { response } = await soapRequest(url, headers, postData, 1000); // Optional timeout parameter(milliseconds)
+  const { body, statusCode } = response;
+  console.log(body);
+  console.log(statusCode);
+})();
